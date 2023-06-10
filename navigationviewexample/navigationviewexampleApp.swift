6f1448b6d1d17a73e9e796e135eebc3f920be3e2
shomil03 <shomil02020@gmail.com>
2023-06-10 22:37:34 +0530

    Initial Commit

---
+++ navigationviewexample/navigationviewexampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  navigationviewexampleApp.swift
+//  navigationviewexample
+//
+//  Created by Shomil Singh on 10/06/23.
+//
+
+import SwiftUI
+
+@main
+struct navigationviewexampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
